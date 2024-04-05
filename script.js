9b21fb9de8293489dabde7299604dc1c1179c70a
lautato15 <lautato15@gmail.com>
2024-04-05 15:30:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,7 @@ function sumMultiples(num) {
 // Crear una función en JavaScript llamada sumMultiples (Sumar Múltiplos) que reciba como parámetro un número natural (entero positivo) y retorne la suma de todos los números múltiplos de 3 o 5, que sean menores al número recibido por parámetro.
 // Si un número fuese múltiplo de 3 y 5, se considerará una sola vez. Ejemplos:
 
-// Input
-// Output
+// Input --------------> Output
 // sumMultiples(1) ---> 0
 // sumMultiples(2) ---> 0
 // sumMultiples(3) ---> 0
